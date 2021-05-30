dfeab72b7dd12e72930704afd0a856b2e1ba501d
karina <71703498+karinaaduu@users.noreply.github.com>
2021-05-30 15:39:14 +0300

    Add files via upload

---
+++ ExceptionsLibrary/Exceptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExceptionsLibrary
+{
+        public class InvalidAccountID : Exception
+        {
+            public InvalidAccountID(string message) : base(message)
+            { }
+        }
+        public class NoEnoughMoney : Exception
+        {
+            public NoEnoughMoney(string message) : base(message)
+            { }
+        }
+        public class ExpenseEsxceedIncome : Exception
+        {
+            public ExpenseEsxceedIncome(string message) : base(message)
+            { }
+        }
+        public class InvalidArgument : Exception
+        {
+            public InvalidArgument(string message) : base(message)
+            { }
+        }
+       public class InvalidIndex : Exception
+       {
+        public InvalidIndex(string message) : base(message)
+            { }
+       }
+}
