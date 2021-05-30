1996e14d60a57b45c95a141090af2a7c974864fb
karina <71703498+karinaaduu@users.noreply.github.com>
2021-05-30 15:41:49 +0300

    Add files via upload

---
+++ IncomeItemLibrary/IncomeItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IncomeItemLibrary
+{
+    public class IncomeItem
+    {
+        public decimal MoneyItemIncome { get; private set; }
+        public string Name { get; private set; }
+        public IncomeItem()
+        {
+            MoneyItemIncome = 0;
+            Name = null;
+        }
+        public IncomeItem(decimal sum, string name)
+        {
+            MoneyItemIncome = sum;
+            Name = name;
+        }
+    }
+}
