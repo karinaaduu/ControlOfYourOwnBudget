2a0b8e507ce4092e4687cb346aefb1284e15aa2f
karina <71703498+karinaaduu@users.noreply.github.com>
2021-05-30 15:41:10 +0300

    Add files via upload

---
+++ AccountEventArgs/AccountEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ExpenseItemLibrary;
+using IncomeItemLibrary;
+
+namespace AccountEventArgsLibrary
+{
+    public class AccountEventArgs
+    {
+      
+        public string Message { get; }
+        public decimal Money { get; }
+        public string Name { get; }
+        public AccountEventArgs(string message, ExpenseItem item)
+        {
+            Message = message;
+            Money=item.MoneyItemExpense;
+            Name = item.Name;
+        }
+        public AccountEventArgs(string message, IncomeItem item)
+        {
+            Message = message;
+            Money = item.MoneyItemIncome;
+            Name = item.Name;
+        }
+        public AccountEventArgs(string message)
+        {
+            Message = message;
+            Name = null;
+            Money = 0;
+        }
+    }
+}
