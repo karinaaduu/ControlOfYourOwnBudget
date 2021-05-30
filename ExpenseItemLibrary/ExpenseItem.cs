97ba7db684222108424cfc178a2fb5ceca39fadd
karina <71703498+karinaaduu@users.noreply.github.com>
2021-05-30 15:39:41 +0300

    Add files via upload

---
+++ ExpenseItemLibrary/ExpenseItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExpenseItemLibrary
+{
+    public class ExpenseItem
+    {
+        public decimal MoneyItemExpense { get; private set; }
+        public string Name { get; private set; }
+        public ExpenseItem()
+        {
+            MoneyItemExpense = 0;
+            Name = null;
+        }
+        public ExpenseItem(decimal sum, string name)
+        {
+            MoneyItemExpense = sum;
+            Name = name;
+        }
+    }
+}
